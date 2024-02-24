3e19a5bf8ab7b0a3cb910c86f78fa91bb70b6e8c
MarioN1010 <mario.navacerrada.arenas10@gmail.com>
2024-02-24 12:31:25 +0100

    se muestra listado de arrays sobre tweets

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-$autor = "Carlos";
-$usuario = "@tripleCarlos";
-$texto = "En lugar de la Mancha de cuyo nombre no quiero acordarme";
+$tweets = [
+    [
+        "autor" => "Carlos",
+        "usuario" => "@tripleCarlos",
+        "texto" => "En lugar de la Mancha de cuyo nombre no quiero acordarme"
+    ],
+    [
+        "autor" => "Juan",
+        "usuario" => "@Juancho",
+        "texto" => "Vivia un loco que veía Molinos"
+    ],
+    [
+        "autor" => "Pepe",
+        "usuario" => "@Pepes",
+        "texto" => "Lorem insu dolor"
+    ]
+];
+
 require_once "index.html";
+
